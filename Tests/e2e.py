2f8ec91a32b968e6d4a155ba536c29467a04cc7a
shlomikaduri <42743238+shlomikaduri@users.noreply.github.com>
2020-09-11 15:31:58 +0300

    test update

--- Tests/e2e.py
+++ Tests/e2e.py
@@ -19,8 +19,8 @@ def scores_test_service(app_url):
     try:
         driver = webdriver.Chrome(executable_path=r'C:\ProjectGames\chromedriver_win32\chromedriver.exe')
     except:
-        print("Error : please check chromedriver location \n\r Required to be locted at C:\ProjectGames\chromedriver_win32\chromedriver.exe")
-        
+        print("------------ Error : please check chromedriver location \n\r Required to be locted at C:\ProjectGames\chromedriver_win32\chromedriver.exe ------------------")
+
 
     driver.implicitly_wait(10)
     driver.get(app_url)
@@ -29,12 +29,18 @@ def scores_test_service(app_url):
     try:
         score = driver.find_element_by_id("score")
         print(score.text)
-        if int(score.text)>=0 and int(score.text)<=1000:
-            return True
-        else:
-            return False
     except:
+        print(
+            "------------ Error : please check chromedriver location \n\r ------------------")
         return False
+
+    if int(score.text)>=0 and int(score.text)<=1000:
+        print("the score in the good range")
+        return True
+    else:
+        print("the score out of the range")
+        return False
+
 
 
 
