afb247560df332b1bff73d7864ec927693390f92
shlomikaduri <42743238+shlomikaduri@users.noreply.github.com>
2020-09-11 15:19:31 +0300

    update test e2e

--- Tests/e2e.py
+++ Tests/e2e.py
@@ -16,18 +16,24 @@ The main function will return -1 as an OS exit code if the tests failed and 0 if
 '''
 
 def scores_test_service(app_url):
+    try:
+        driver = webdriver.Chrome(executable_path=r'C:\ProjectGames\chromedriver_win32\chromedriver.exe')
+    except:
+        print("Error : please check chromedriver location \n\r Required to be locted at C:\ProjectGames\chromedriver_win32\chromedriver.exe")
+        exit()
 
-    driver = webdriver.Chrome(executable_path=r'C:\ProjectGames\chromedriver_win32\chromedriver.exe')
     driver.implicitly_wait(10)
-
     driver.get(app_url)
-
     print(driver.title)
-    score = driver.find_element_by_id("score")
-    print(score.text)
-    if int(score.text)>=0 and int(score.text)<=1000:
-        return True
-    else:
+
+    try:
+        score = driver.find_element_by_id("score")
+        print(score.text)
+        if int(score.text)>=0 and int(score.text)<=1000:
+            return True
+        else:
+            return False
+    except:
         return False
 
 
@@ -45,7 +51,7 @@ def main_function():
         return 0
     else:
         print("Fail")
-        raise Exception('Test fail')
+        return -1
 
 main_function()
 
